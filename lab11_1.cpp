a96318e4bebf802479a58b3689bba72c1b427eaa
khachenchai <jaiklaanda@gmail.com>
2025-12-26 15:21:42 +0700

    Finished Lab11-1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,30 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    int i = 0, ran;
+    string grade, wow;
+    while (i < 3) {
+        getline(cin, wow);
+        i++;
+    }
+    ran = rand() % 9 + 0;
+    if (ran == 0) grade = "A";
+    else if (ran == 1) grade = "B+";
+    else if (ran == 2) grade = "B";
+    else if (ran == 3) grade = "C+";
+    else if (ran == 4) grade = "C";
+    else if (ran == 5) grade = "D+";
+    else if (ran == 6) grade = "D";
+    else if (ran == 7) grade = "F";
+    else if (ran == 8) grade = "W";
+
+    cout << "You will get " << grade << " in this 261102.";
+    return 0;
+}
